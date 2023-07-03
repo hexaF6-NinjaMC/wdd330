1bc08eabc810abaa5f8e1466dcc085dae5d6593c
hexaF6-NinjaMC <103971610+hexaF6-NinjaMC@users.noreply.github.com>
2023-07-03 11:05:18 -0500

    Update ls.js - removal Func
    Added removal function for LocalStorage todo list array.

--- todo/js/ls.js
+++ todo/js/ls.js
@@ -16,3 +16,11 @@ export function writeToLS(key, data) {
     let dataSerialized = JSON.stringify(data);
     window.localStorage.setItem(key, dataSerialized);
 }
+
+export function removeLS(key) {
+    /*
+    remove (clear) an array (if it exists) of objects in LocalStorage under the provided key.
+    key (String): The key under which the value is stored in LS.
+    */
+    window.localStorage.removeItem(key);
+}
